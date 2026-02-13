835b3241490ae75d0910098a94a3c11f8c454946
SHANIGARAM MANOJ <manojshanigaram143@gmail.com>
2026-02-13 15:10:10 +0530

    Time: 1 ms (99.91%) | Memory: 46.8 MB (80.95%) - LeetSync

---
+++ 4008-restore-finishing-order/restore-finishing-order.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] recoverOrder(int[] order, int[] friends) {
+        int[] arr=new int[friends.length];
+        int x=0;
+        for(int i=0;i<order.length;i++){
+            for(int j=0;j<friends.length;j++){
+                if(order[i]==friends[j]) arr[x++]=friends[j];
+            }
+        }
+        return arr;
+    }
+}
