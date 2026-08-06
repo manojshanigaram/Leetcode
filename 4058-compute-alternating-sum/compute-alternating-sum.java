0a3dfa20dc853eecd606a56a52a424fcfbbb11f9
SHANIGARAM MANOJ <manojshanigaram143@gmail.com>
2026-08-06 23:04:13 +0530

    Time: 1 ms (97.38%) | Memory: 46.3 MB (65.94%) - LeetSync

---
+++ 4058-compute-alternating-sum/compute-alternating-sum.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int sum=0,odd=0;
+        if(nums.length==1) return nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(i%2!=0) odd+=nums[i];
+            else sum+=nums[i];
+        }
+        return sum-odd;
+    }
+}
