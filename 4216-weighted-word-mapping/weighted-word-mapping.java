11217debca928078b712bee44c14f71386f75bbf
SHANIGARAM MANOJ <manojshanigaram143@gmail.com>
2026-08-06 22:40:48 +0530

    Time: 2 ms (93.36%) | Memory: 46.6 MB (30.91%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder ans = new StringBuilder();
+        for (String word : words) {
+            int sum = 0;
+            for (char ch : word.toCharArray()) {
+                sum += weights[ch - 'a'];
+            }
+            int rem = sum % 26;
+            char mapped = (char) ('z' - rem);
+            ans.append(mapped);
+        }
+        return ans.toString();
+    }
+}
