4404a38aa5518c2c928024ac4bb92bda48c25215
SHANIGARAM MANOJ <manojshanigaram143@gmail.com>
2026-09-08 19:21:00 +0530

    Time: 510 ms (34.12%) | Memory: 0B (100.00%) - LeetSync

---
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select max(num) as num from MyNumbers
+    where num in(
+        select num
+        FROM MyNumbers
+        GROUP BY num
+        HAVING COUNT(num) = 1
+    );
