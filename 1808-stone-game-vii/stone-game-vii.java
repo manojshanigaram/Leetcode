f70f30e5dc0de466a0f04c7e7082ff7764d91161
SHANIGARAM MANOJ <manojshanigaram143@gmail.com>
2026-09-13 08:46:18 +0530

    Time: 492 ms (13.85%) | Memory: 291.2 MB (5.03%) - LeetSync

--- 1808-stone-game-vii/stone-game-vii.java
+++ 1808-stone-game-vii/stone-game-vii.java
@@ -9,20 +9,20 @@ class Solution {
             preSum[i + 1] = preSum[i] + stones[i];
         return dp(0, n - 1, 1);
     }
-    int getSum(int left, int r) {
-        return preSum[r + 1] - preSum[left];
+    int getSum(int l, int r) {
+        return preSum[r + 1] - preSum[l];
     }
-    int dp(int left, int r, int isAlice) {
-        if (left == r) return 0;
-        if (memo[left][r][isAlice] != null) return memo[left][r][isAlice];
+    int dp(int l, int r, int isAlice) {
+        if (l == r) return 0;
+        if (memo[l][r][isAlice] != null) return memo[l][r][isAlice];
         if (isAlice == 1) {
-            int a = dp(left + 1, r, 1 - isAlice) + getSum(left + 1, r);
-            int b = dp(left, r - 1, 1 - isAlice) + getSum(left, r - 1);
-            return memo[left][r][isAlice] = Math.max(a, b);
+            int a = dp(l + 1, r, 1 - isAlice) + getSum(l + 1, r);
+            int b = dp(l, r - 1, 1 - isAlice) + getSum(l, r - 1);
+            return memo[l][r][isAlice] = Math.max(a, b);
         } else {
-            int a = dp(left + 1, r, 1 - isAlice) - getSum(left + 1, r); // Take leftmost
-            int b = dp(left, r - 1, 1 - isAlice) - getSum(left, r - 1); // Take rmost
-            return memo[left][r][isAlice] = Math.min(a, b);
+            int a = dp(l + 1, r, 1 - isAlice) - getSum(l + 1, r);
+            int b = dp(l, r - 1, 1 - isAlice) - getSum(l, r - 1);
+            return memo[l][r][isAlice] = Math.min(a, b);
         }
     }
 }
